1cddbfbdb0080dab33ab614ff35f88e15dc9ca56
Dmitry Shuvalov <mitiashuv74@mail.ru>
2019-05-05 13:06:59 +0300

    Intro to JS

---
+++ JSIN-001/index.js
@@ -0,0 +1,17 @@
+// URL to kodaktor: https://kodaktor.ru/?!=jsin_53062
+
+// Form fahrenheit to  celsius
+const fDeg = 32;
+const cDeg = ((fDeg - 32) * 5) / 9;
+
+// From roubles to dollars
+const roubles = 25000;
+const dollars = 25000 / 66;
+
+// Quadratic equation
+const a = 8;
+const b = 45;
+const c = -18;
+const D = Math.pow(b, 2) - 4 * a * c;
+const x1 = (-b + Math.sqrt(D)) / (2 * a);
+const x2 = (-b - Math.sqrt(D)) / (2 * a);
